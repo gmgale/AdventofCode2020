dbb27115fbc6aac726e7bd949a284ab71d344a94
gmgale <gmgale@icloud.com>
2020-12-15 12:03:26 +0100

    Update

--- Day_14/pt1.py
+++ Day_14/pt1.py
@@ -3,7 +3,7 @@ import re
 testData = open(r'C:\Users\gmgal\go\src\github.com\gmgale\AdventofCode2020\Day_14\testData.txt',"r")
 
 memory = []
-for i in range(2^32):
+for i in range(35):
     memory.append(0)
 
 mask = []
@@ -19,10 +19,11 @@ while EOF != True:
 
         
         if d[0:4] == "mask":
-            m = d[7:]
+            m = d[7:-2]
             for i in range(len(m)):
                 mask.append(m[i])
             break
+
 
         address = 0
         out = re.compile('\[.\]')
@@ -30,32 +31,37 @@ while EOF != True:
         if mr:
             address = str(mr.group(0))
             address = address[1: len(address) -1]
+        address = int(address)
 
         value = 0
         out = re.compile('=.+')
         mr = out.search(d)
         if mr:
             value = str(mr.group(0))
-            value = str(bin(int(value[2:])))
+            valueArr = str(bin(int(value[1:]))).split()
 
             m = []
-            for i in range((2^32)-(len(value)-2)):
+            for i in range((37)-(len(value)-2)):
                 m.append(0)
 
             for i in range(2, len(value)):
                 m.append(int(value[i]))
-
+            value = m[2:]
 
         # Now apply Mask to Value and save to memory[address]
-
-
         for i in range(len(value)):
-            for j in range(len(value)):
-
-        
-    
-        
-
-        
+            if mask[i] == "X":
+                memory[i] = int(value[i])
+                continue
+            if mask[i] == value[i]:
+                print("Copy")
+                memory[i] = int(value[i])
+                continue
+            if mask[i] != value[i]:
+                print("mask")
+                memory[i] = int(mask[i])
+                continue
+            
+        print(memory)
 
 
